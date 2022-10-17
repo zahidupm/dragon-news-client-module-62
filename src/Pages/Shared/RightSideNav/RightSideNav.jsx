14593f7a9a125382fdaf5c6b5efbd7dcdf794bf7
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-18 00:33:00 +0600

    create layout folder structure and basic components

---
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RightSideNav = () => {
+    return (
+        <div>
+            <h2>Right side nav</h2>
+        </div>
+    );
+};
+
+export default RightSideNav;
