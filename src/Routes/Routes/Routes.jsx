b38ec8b7b697fce807be13601153970ba0a6062a
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-19 22:14:08 +0600

    load news category data from server

--- src/Routes/Routes/Routes.jsx
+++ src/Routes/Routes/Routes.jsx
@@ -9,9 +9,21 @@ export const routes = createBrowserRouter([
         path: '/',
         element: <Main></Main>,
         children: [
-            {path: '/', element: <Home></Home>},
-            {path: '/category/:id', element: <Category></Category>},
-            {path: '/news/:id', element: <News></News>},
+            {
+                path: '/', 
+                element: <Home></Home>,
+                loader: () => fetch(`http://localhost:5000/news`)
+            },
+            {
+                path: '/category/:id', 
+                element: <Category></Category>,
+                loader: ({params}) => fetch(`http://localhost:5000/category/${params.id}`)
+             },
+            {
+                path: '/news/:id', 
+                element: <News></News>,
+                loader: ({params}) => fetch(`http://localhost:5000/news/${params.id}`)
+            },
         ]
     }
 ])
