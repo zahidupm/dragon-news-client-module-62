11bc45f5d4d3c75a544b6b489f19d258e6190e14
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-20 00:45:28 +0600

    implement google sign in and display user info

--- src/Pages/Shared/RightSideNav/RightSideNav.jsx
+++ src/Pages/Shared/RightSideNav/RightSideNav.jsx
@@ -1,15 +1,31 @@
-import React from 'react';
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext } from 'react';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { FaFacebook, FaGithub, FaGoogle, FaTwitch, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 import BrandCarousel from '../BrandCarousel/BrandCarousel';
 
 const RightSideNav = () => {
+
+    const {providerLogin} = useContext(AuthContext);
+
+    const googleAuthProvider = new GoogleAuthProvider();
+    
+    const handleGoogleSignIn = () => {
+        providerLogin(googleAuthProvider)
+        .then(result => {
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.error(error));
+    }
+
     return (
         <div>
             <ButtonGroup vertical>
-                <Button className='mb-2' variant='outline-primary'><FaGoogle></FaGoogle> Login with Google</Button>
+                <Button onClick={handleGoogleSignIn} className='mb-2' variant='outline-primary'><FaGoogle></FaGoogle> Login with Google</Button>
                 <Button variant='outline-dark'><FaGithub></FaGithub> Login with Github</Button>
             </ButtonGroup>
             <div className='mt-4'>
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,11 +1,29 @@
-import React, { createContext } from 'react';
+import { getAuth, onAuthStateChanged, signInWithPopup } from 'firebase/auth';
+import React, { createContext, useEffect, useState } from 'react';
+import app from '../../firebase/firebase.config';
 
 export const AuthContext = createContext();
 
-const AuthProvider = ({children}) => {
-    const user = {displayName: 'Zahid'};
+const auth = getAuth(app)
 
-    const authInfo = {user};
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+    const providerLogin = (provider) => {
+        signInWithPopup(auth, provider);
+    }
+
+    useEffect( () => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            console.log('user inside state change', currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unsubscribe();
+        }
+    }, [])
+
+    const authInfo = {user, providerLogin};
 
     return (
         <AuthContext.Provider value={authInfo}>
