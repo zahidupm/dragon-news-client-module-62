df417a3f2fef2744c2f257074fd80abcea05af2e
zahidupm <zahidhasanlimon111@gmail.com>
2022-10-20 22:17:35 +0600

    loading state set and fixed reload problem

--- src/Routes/PrivateRoute/PrivateRoute.js
+++ src/Routes/PrivateRoute/PrivateRoute.js
@@ -1,10 +1,15 @@
 import React, { useContext } from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import { Navigate, useLocation } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const PrivateRoute = ({children}) => {
-    const {user} = useContext(AuthContext);
+    const {user, loading} = useContext(AuthContext);
     const location = useLocation();
+
+    if(loading) {
+        return <Spinner animation="border" variant="primary" />
+    }
 
     if(!user) {
         return <Navigate to='/login' state={{from: location}} replace></Navigate>
--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -8,20 +8,25 @@ const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const providerLogin = (provider) => {
+        setLoading(true);
         signInWithPopup(auth, provider);
     }
 
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     }
 
     const signIn = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logOut = () => {
+        setLoading(true);
         return signOut(auth);
     }
 
@@ -29,13 +34,14 @@ const AuthProvider = ({children}) => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             console.log('user inside state change', currentUser);
             setUser(currentUser);
+            setLoading(false);
         });
         return () => {
             unsubscribe();
         }
     }, [])
 
-    const authInfo = {user, providerLogin, logOut, createUser, signIn};
+    const authInfo = {user, providerLogin, logOut, createUser, signIn, loading};
 
     return (
         <AuthContext.Provider value={authInfo}>
